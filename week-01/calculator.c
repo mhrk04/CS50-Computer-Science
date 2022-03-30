b8db948bc358a69bb6f5de3f7b4863ef61941025
mhrk04 <haziqrohaizan@gmail.com>
2022-03-30 19:17:48 +0800

    test

--- week-01/calculator.c
+++ week-01/calculator.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    int x = get_int("x : ");
-    int y = get_int("y : ");
-    printf("%i\n", x+y);
+    int x = get_int("x: ");
+    int y = get_int("y: ");
+    printf("%i\n", x + y);
 }
